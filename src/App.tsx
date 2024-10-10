2dd587aa1520fa1348d9b51487b6fc23d1c2f7f0
KiranKrishna004 <kiran.004narath@gmail.com>
2024-10-10 22:45:08 +0530

    feat: add error state to input field

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import "./App.css"
 import { CustomInputField } from "./components/CustomInputField"
 
 function App() {
-  return <CustomInputField label="First Name" />
+  return (
+    <CustomInputField label="First Name" errorMessage="Provide First Name" />
+  )
 }
 
 export default App
--- src/components/CustomInputField/index.tsx
+++ src/components/CustomInputField/index.tsx
@@ -1,10 +1,17 @@
 import "./styles.css"
 
-export const CustomInputField = ({ label }: { label: string }) => {
+export const CustomInputField = ({
+  label,
+  errorMessage,
+}: {
+  label: string
+  errorMessage: string
+}) => {
   return (
-    <label className="custom-field one">
+    <label className={`custom-field one ${errorMessage ? "has-error" : ""}`}>
       <input type="text" placeholder=" " />
       <span className="placeholder">{label}</span>
+      <span className="tooltiptext">{errorMessage}</span>
     </label>
   )
 }
