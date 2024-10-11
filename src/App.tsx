39483d9dba11d89ac3c319baf9c4fd514364ee4b
KiranKrishna004 <kiran.004narath@gmail.com>
2024-10-12 01:58:30 +0530

    feat: update navbar and herosection

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react"
 import "./App.css"
 import { HeroSection } from "./components/HeroSection"
 import { ImageSection } from "./components/ImageSection"
-import { Navbar } from "./components/Navbar"
 
 function App() {
-  const [page, setPage] = useState("/")
   return (
     <div>
-      <Navbar setPage={setPage} page={page} />
       <HeroSection />
       <ImageSection />
     </div>
--- src/components/ImageSection/index.tsx
+++ src/components/ImageSection/index.tsx
@@ -1,8 +1,14 @@
 import "./styles.css"
+import img1 from "../../assets/Imagesection/img1.png"
+import img2 from "../../assets/Imagesection/img2.png"
+import img3 from "../../assets/Imagesection/img3.png"
+
 export const ImageSection = () => {
   return (
     <div className="image-section">
-      <img />
+      <img src={img1} alt="img1 missing" height={180} />
+      <img src={img2} alt="img2 missing" height={180} />
+      <img src={img3} alt="img3 missing" height={180} />
     </div>
   )
 }
