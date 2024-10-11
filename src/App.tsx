1b6406d1cd6ca9706cde107caebe0f50a4a37a55
KiranKrishna004 <kiran.004narath@gmail.com>
2024-10-12 01:30:35 +0530

    refactor: modularize ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import "./App.css"
-import { Navbar } from "./components/Navbar"
 import { HeroSection } from "./components/HeroSection"
+import { ImageSection } from "./components/ImageSection"
+import { Navbar } from "./components/Navbar"
 
 function App() {
   const [page, setPage] = useState("/")
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Navbar setPage={setPage} page={page} />
       <HeroSection />
+      <ImageSection />
     </div>
   )
 }
---
+++ src/components/ImageSection/index.tsx
@@ -0,0 +1,8 @@
+import "./styles.css"
+export const ImageSection = () => {
+  return (
+    <div className="image-section">
+      <img />
+    </div>
+  )
+}
